5ec97c7eedd830ec7abcbe82f7719c6ec188ec89
devzhong <devzhong@163.com>
2020-07-31 23:57:18 +0800

    jenkins build test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App hh"/>
+    <HelloWorld msg="jenkins imamba build test"/>
   </div>
 </template>
 
